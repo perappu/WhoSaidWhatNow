b308cfd937fd6160afd651cce328a9b8f7cc4b07
erunseelie <erunseelie+git@pm.me>
2023-09-03 11:35:24 -0500

    initial search bar

--- WhoSaidWhatNow/Windows/TabIndividual.cs
+++ WhoSaidWhatNow/Windows/TabIndividual.cs
@@ -17,6 +17,7 @@ public class TabIndividual
 
         if (ImGui.BeginTabItem("Individual"))
         {
+            var players = new Player[Plugin.Players.Count];
 
             //janky handling for if we're coming back to this tab from the groups tab
             if (Plugin.SelectedPlayer is null)
@@ -27,6 +28,10 @@ public class TabIndividual
             // Creating left and right panels
             // you can redeclare BeginChild() with the same ID to add things to them, which we do for chatlog
             ImGui.BeginChild(MainWindow.ID_PANEL_LEFT, new Vector2(230 * ImGuiHelpers.GlobalScale, 0), true, ImGuiWindowFlags.MenuBar);
+
+            var input = "";
+            ImGui.InputTextWithHint("", "Filter by name...", ref input, 40, ImGuiInputTextFlags.EnterReturnsTrue);
+            Plugin.Players.Where(p => p.Name.ToLower().Contains(input.ToLower())).ToList().CopyTo(players);
 
             if (ImGui.BeginMenuBar())
             {
@@ -129,10 +134,18 @@ public class TabIndividual
 
 
             //Reopen left window, populate selectable list
-            foreach (var p in Plugin.Players)
+            foreach (var p in players)
             {
                 ImGui.BeginChild(MainWindow.ID_PANEL_LEFT);
-                mainWindow.AddPlayerSelectable(p);
+                // catch NPEs silently
+                try
+                {
+                    mainWindow.AddPlayerSelectable(p);
+                }
+                catch
+                {
+
+                }
                 ImGui.EndChild();
             }
 
