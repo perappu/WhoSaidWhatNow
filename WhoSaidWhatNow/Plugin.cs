3f11c56722297a8aef8fe4773cec9cf3e4611b5a
Stokori <stokoriel@gmail.com>
2024-07-23 17:00:07 -0500

    chore: remove obsolete [RequiredVersion]

--- WhoSaidWhatNow/Plugin.cs
+++ WhoSaidWhatNow/Plugin.cs
@@ -36,39 +36,30 @@ namespace WhoSaidWhatNow
         public ConfigWindow ConfigWindow { get; }
 
         [PluginService]
-        [RequiredVersion("1.0")]
         public static DalamudPluginInterface PluginInterface { get; private set; } = null!;
 
         [PluginService]
-        [RequiredVersion("1.0")]
         public static ICommandManager CommandManager { get; private set; } = null!;
 
         [PluginService]
-        [RequiredVersion("1.0")]
         public static IDataManager DataManager { get; private set; } = null!;
 
         [PluginService]
-        [RequiredVersion("1.0")]
         public static ITargetManager TargetManager { get; private set; } = null!;
 
         [PluginService]
-        [RequiredVersion("1.0")]
         public static IClientState ClientState { get; private set; } = null!;
 
         [PluginService]
-        [RequiredVersion("1.0")]
         public static IChatGui ChatGui { get; private set; } = null!;
 
         [PluginService]
-        [RequiredVersion("1.0")]
         public static IObjectTable ObjectTable { get; private set; } = null!;
 
         [PluginService]
-        [RequiredVersion("1.0")]
         public static IGameConfig GameConfig { get; private set; } = null!;
 
         [PluginService]
-        [RequiredVersion("1.0")]
         public static IPluginLog Logger { get; private set; } = null!;
 
         public static FileDialogManager FileDialogManager { get; set; } = new FileDialogManager();
